9b28b56460255ae84535bb5a83bc23aed276dd6e
erinknight242 <erinknight242@gmail.com>
2023-03-06 21:01:30 -0600

    Remove azure workflow, prep for aws

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const helloWorld = require('./scripts/helloWorld');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: false,
+  socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
   // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
   // you still need to listen on some port!
