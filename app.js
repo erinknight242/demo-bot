36dbc8feff0da7c63fc790adcd25a77c9fcdc54a
erinknight242 <erinknight242@gmail.com>
2023-03-06 23:08:45 -0600

    Remove logs

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ let secret;
 let app;
 
 (async () => {
-  console.log("IIFE - before try");
   try {
     response = await client.send(
       new GetSecretValueCommand({
@@ -25,22 +24,17 @@ let app;
       })
     );
   } catch (error) {
-    console.log('*** In the catch');
-    console.log(error);
     // For a list of exceptions thrown, see
     // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
     throw error;
   }
   
   secret = JSON.parse(response.SecretString);
-  console.log(secret);
   startApp();
 })();
 
 
 function startApp() {
-  console.log('starting app');
-  
   app = new App({
     token: secret.SLACK_BOT_TOKEN,
     signingSecret: secret.SLACK_SIGNING_SECRET,
@@ -49,9 +43,7 @@ function startApp() {
   });
   
   (async () => {
-    // Start your app
-    console.log('await app')
-    await app.start(process.env.PORT || 3000);
+    await app.start();
   
     console.log('⚡️ Bolt app is running!');
   
